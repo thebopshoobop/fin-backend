714e45823fc6f959536943c8a9c435b70a270d32
Will Timpson <will.timpson@gmail.com>
2017-12-02 02:45:31 -0500

    model prep

--- fin_backend/api/hello.py
+++ fin_backend/api/hello.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from fin_backend.models import db, User
 
 hello = Blueprint("hello", __name__)
 
@@ -6,4 +7,15 @@ hello = Blueprint("hello", __name__)
 @hello.route("/", defaults={"name": "World"})
 @hello.route("/<name>")
 def show(name):
+    if name != "World":
+        user = User(name=name)
+        db.session.add(user)
+        db.session.commit()
+
     return f"Hello, {name}!"
+
+
+@hello.route("/users")
+def shows():
+    users = User.query.all()
+    return ', '.join([user.name for user in users])
--- fin_backend/app.py
+++ fin_backend/app.py
@@ -5,8 +5,9 @@ def create_app(config_filename):
     app = Flask(__name__)
     # app.config.from_pyfile(config_filename)
 
-    # from yourapplication.model import db
-    # db.init_app(app)
+    from fin_backend.models import db
+    db.init_app(app)
+    db.create_all(app=app)
 
     from fin_backend.api.hello import hello
     app.register_blueprint(hello, url_prefix="/api")
